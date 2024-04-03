c1e936c953e4b4623565b1798c4a5d2995c46d81
Adam Dohojda <dohojda.adam@gmail.com>
2024-04-03 20:01:12 +0200

    poprawiony pipeline, wydaje na zewnątrz krotkę

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 
 def transform_data(X : pd.DataFrame):
+    X = X.copy()
+    X.set_index(['Customer_id'], inplace=True)
     real_variables_columns = pd.read_excel('Data_dictionary.xlsx').iloc[:42, :]
     types = {k:[] for k in real_variables_columns['Type'].unique()}
 
     real_variables_columns
     for feature in real_variables_columns.iterrows():
         # all variables with x on the end just land with 1-12
+        if feature[1]['Column name*'] == 'Customer_id': continue
         if(feature[1]['Column name*'][-1] =='x'):
             for lag in range(13):
                 types[feature[1]['Type']].append((feature[1]['Column name*'][:-1]+str(lag)).replace(' ', '_'))
@@ -15,4 +18,5 @@ def transform_data(X : pd.DataFrame):
             types[feature[1]['Type']].append(feature[1]['Column name*'].replace(' ', '_'))
 
     # at this time we include only numeric features
-    return X[types['Integer'] + types['Float'] + types['Integer (0-330)'] + types['Integer (0 or 1)']]
+    return X[types['Integer'] + types['Float'] + types['Integer (0-330)'] + types['Integer (0 or 1)']], X['Target']
+
